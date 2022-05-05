be456d7e5eec0ad22b9965a7f6c2aad1742e0e9d
Poga Kuofie <mail@pogakuofie.com>
2022-05-05 09:08:28 +0000

    chore: manifest update grammer correction

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <p>Delivea - by product Mimi, Kohol, Poga &amp; Clement</p>
+      <p>Delivea - product by Mimi, Kohol, Poga &amp; Clement</p>
     </div>
   );
 }
